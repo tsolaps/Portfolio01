65ac5a4d70767411ee945696a939271d0670b134
Tsola <tsolapatrick@gmail.com>
2026-07-09 14:40:19 +0100

    added vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
     build: {
         outDir: "dist",
         assetsDir: "assets",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                projects: resolve(__dirname, "projects.html"),
+            },
+        },
     },
 });
