bc6007873d54e5faba6c4da8d2c3480ba41193e8
Tsola <tsolapatrick@gmail.com>
2026-07-13 00:00:28 +0100

    fix: set base to relative path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
+    base: "./",
     build: {
         outDir: "dist",
         assetsDir: "assets",
